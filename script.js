49734096ccd3a2e44107b7f0802b950d739998f7
Kristina Klyukova <klyukova.k@yandex.ru>
2023-01-10 12:11:12 +0300

    p

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const formElement = document.querySelector(".popup");
-const formImgElement = document.querySelector(".popup__images");
-const nameInput = formElement.querySelector("#name");
-const namePlaceInput = formElement.querySelector("#nameplace");
-const jobInput = formElement.querySelector("#profession");
-const imagesInput = formElement.querySelector("#images");
+const formElement = document.querySelectorAll(".popup");
+const nameInput = formElement[0].querySelector("#name");
+const namePlaceInput = formElement[1].querySelector("#nameplace");
+const jobInput = formElement[0].querySelector("#profession");
+const imagesInput = formElement[1].querySelector("#images");
 const editButton = document.querySelector(".profile-info__button");
 const addButton = document.querySelector(".profile__button");
 const closeButton = document.querySelectorAll(".popup__close");
@@ -16,18 +15,18 @@ function formSubmitHandler(evt) {
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 }
-formElement.addEventListener("submit", formSubmitHandler);
+formElement[0].addEventListener("submit", formSubmitHandler);
 
   editButton.addEventListener('click', function openPopup() {
-    formElement.classList.add("popup_open");
+    formElement[0].classList.add("popup_open");
   });
   closeButton[0].addEventListener('click', function closePopup() {
-    formElement.classList.remove("popup_open");
+    formElement[0].classList.remove("popup_open");
   });
 
   addButton.addEventListener('click', function openPopup() {
-    formImgElement.classList.add("popup_open");
+    formElement[1].classList.add("popup_open");
   });
   closeButton[1].addEventListener('click', function closePopup() {
-    formImgElement.classList.remove("popup_open");
+    formElement[1].classList.remove("popup_open");
   });
