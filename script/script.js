6cc824b8b4e655a010e139b0888577d448a4e269
Klyukova Kristins <klyukova.k@yandex.ru>
2023-01-27 16:05:43 +0300

    добавление карточек

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ const editButton = document.querySelector(".profile-info__button");
 const addButton = document.querySelector(".profile__button");
 const closeButton = document.querySelectorAll(".popup__close");
 const likeButton = document.querySelectorAll(".element__like");
-
+const elementContainer = document.querySelector('.elements');
 /*открытие и закрытие popup1*/
 editButton.addEventListener('click', function openPopup() {
   formElement[0].classList.add("popup_open");
@@ -24,6 +24,9 @@ function formSubmitHandler(evt) {
 }
 formElement[0].addEventListener("submit", formSubmitHandler);
 
+formElement[0].querySelector('.popup__button').addEventListener('click', function closePopup() {
+  formElement[0].classList.remove("popup_open");
+});
 /*открытие и закрытие popup2*/
 addButton.addEventListener('click', function openPopup() {
   formElement[1].classList.add("popup_open");
@@ -31,32 +34,32 @@ addButton.addEventListener('click', function openPopup() {
 closeButton[1].addEventListener('click', function closePopup() {
   formElement[1].classList.remove("popup_open");
 });
+
 /*добавление карточек*/
-function addImage(elementImageValue,elementTextValue) {
+function addImage(elementImageValue, elementTextValue) {
   const card = document.querySelector('#element').content;
   const element = card.querySelector('.element').cloneNode('true');
   element.querySelector('.element__image').src = elementImageValue;
   element.querySelector('.element__text').textContent = elementTextValue;
-
+  element.querySelector('.element__like').addEventListener('click', function (evt) {
+    const eventTarget = evt.target;
+    evt.target.classList.toggle('element__like_active');
+  });
+  element.querySelector('.element__delete').addEventListener('click', function (){
+    element.remove();
+  });
   elementContainer.append(element);
 };
+
 /*сохраниение popup2*/
-function formNewPlaces(evt) {
-  evt.preventDefault();
+formElement[1].querySelector('.popup__button').addEventListener('click', function () {
   const elementText = formElement[1].querySelector("#nameplace");
   const elementImage = formElement[1].querySelector("#images");
+  addImage(elementImage.value, elementText.value);
+  elementImage.value = '';
+  elementText.value = '';
+});
 
-  addImage(elementImage.value,elementText.value);
-}
-formElement[1].addEventListener("submit", formNewPlaces);
-/*
-addButton.addEventListener('click', function () {
-  const artist = document.querySelector('.input__text_type_artist');
-  const title = document.querySelector('.input__text_type_title');
-
-  addSong(artist.value, title.value);
-  renderHasSongs();
-
-  artist.value = '';
-  title.value = '';
-});*/
+formElement[1].querySelector('.popup__button').addEventListener('click', function closePopup() {
+  formElement[1].classList.remove("popup_open");
+});
